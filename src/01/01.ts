0a05a6741eb8e814265b571c457fc695bb0a3c87
Bertalan Körmendy <korber314@gmail.com>
2024-12-01 06:20:59 +0100

    cleanup

--- src/01/01.ts
+++ src/01/01.ts
@@ -1,19 +1,20 @@
 import { lines, sum, words, zip } from '@/advent'
 
-export function parse(input: string) {
-  return lines(input).map(l => words(l).map(Number))
+export function parse(input: string): [number[], number[]] {
+  const rows = lines(input).map(l => words(l).map(Number))
+  return [rows.map(r => r.at(0)!), rows.map(r => r.at(1)!)]
 }
 
 type Input = ReturnType<typeof parse>
 
 export function partOne(input: Input) {
-  const f = input.map(i => i.at(0)!).sort()
-  const s = input.map(i => i.at(1)!).sort()
+  const f = input[0].sort()
+  const s = input[1].sort()
   return sum([...zip(f, s)].map(([a, b]) => Math.abs(a - b)))
 }
 
 export function partTwo(input: Input) {
-  const f = input.map(i => i.at(0)!)
-  const s = input.map(i => i.at(1)!)
-  return sum(f.map(n => s.filter(nn => nn === n).length * n))
+  const f = input[0].sort()
+  const s = input[1].sort()
+  return sum(f.map(n => n * s.filter(nn => nn === n).length))
 }
