201eca4620c1c49974197ffc567f911afa484b8d
Bertalan Körmendy <korber314@gmail.com>
2024-12-01 07:25:30 +0100

    might as well sort in parse

--- src/01/01.ts
+++ src/01/01.ts
@@ -2,19 +2,17 @@ import { lines, sum, words, zip } from '@/advent'
 
 export function parse(input: string): [number[], number[]] {
   const rows = lines(input).map(l => words(l).map(Number))
-  return [rows.map(r => r.at(0)!), rows.map(r => r.at(1)!)]
+  return [rows.map(r => r.at(0)!).sort(), rows.map(r => r.at(1)!).sort()]
 }
 
 type Input = ReturnType<typeof parse>
 
 export function partOne(input: Input) {
-  const f = input[0].sort()
-  const s = input[1].sort()
+  const [f, s] = input
   return sum([...zip(f, s)].map(([a, b]) => Math.abs(a - b)))
 }
 
 export function partTwo(input: Input) {
-  const f = input[0].sort()
-  const s = input[1].sort()
+  const [f, s] = input
   return sum(f.map(n => n * s.filter(nn => nn === n).length))
 }
