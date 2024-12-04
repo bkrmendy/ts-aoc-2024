394a16a8897adaa7cd930e40cb4aade8d42e4d1b
Bertalan Körmendy <korber314@gmail.com>
2024-12-04 08:16:18 +0100

    d2

--- src/04/04.ts
+++ src/04/04.ts
@@ -13,19 +13,24 @@ function rotate90(input: Input) {
 const inBounds = (input: Input, [row, col]: [number, number]) =>
   row >= 0 && row < input.length && col >= 0 && col < input[0]!.length
 
-function rotate45(input: Input): Input {
-  const step = ([row, col]: [number, number]): [number, number] => [
-    row + 1,
-    col + 1
-  ]
+const stepDownRight = ([row, col]: [number, number]): [number, number] => [
+  row + 1,
+  col + 1
+]
 
+const stepDownLeft = ([row, col]: [number, number]): [number, number] => [
+  row + 1,
+  col - 1
+]
+
+function rotate45(input: Input): Input {
   const result: Input = []
   for (let iCol = input.at(0)!.length - 1; iCol >= 0; iCol--) {
     let pos: [number, number] = [0, iCol]
     let row: string[] = []
     while (inBounds(input, pos)) {
       row.push(input.at(pos[0])!.at(pos[1])!)
-      let [nr, nc] = step(pos)
+      let [nr, nc] = stepDownRight(pos)
       pos = [nr, nc]
     }
     result.push(row)
@@ -36,7 +41,7 @@ function rotate45(input: Input): Input {
     let row: string[] = []
     while (inBounds(input, pos)) {
       row.push(input.at(pos[0])!.at(pos[1])!)
-      let [nr, nc] = step(pos)
+      let [nr, nc] = stepDownRight(pos)
       pos = [nr, nc]
     }
     result.push(row)
@@ -107,4 +112,35 @@ export function partOne(input: Input) {
   ])
 }
 
-export function partTwo(input: Input) {}
+function get(input: Input, [row, col]: [number, number]) {
+  return input.at(row)?.at(col)
+}
+
+function hasXMAS(input: Input, [row, col]: [number, number]): boolean {
+  const a = [
+    get(input, [row, col]),
+    get(input, stepDownRight([row, col])),
+    get(input, stepDownRight(stepDownRight([row, col])))
+  ].join('')
+
+  const b = [
+    get(input, [row, col + 2]),
+    get(input, stepDownLeft([row, col + 2])),
+    get(input, stepDownLeft(stepDownLeft([row, col + 2])))
+  ].join('')
+
+  return (a === 'MAS' || a === 'SAM') && (b === 'MAS' || b === 'SAM')
+}
+
+export function partTwo(input: Input) {
+  const [rows, cols] = [input.length, input.at(0)!.length]
+  let total = 0
+  for (const iRow of range(0, rows)) {
+    for (const iCol of range(0, cols)) {
+      if (hasXMAS(input, [iRow, iCol])) {
+        total += 1
+      }
+    }
+  }
+  return total
+}
