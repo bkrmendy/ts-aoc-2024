f2280fb19fc17796d210b23825b0f5085bf8809b
Bertalan Körmendy <korber314@gmail.com>
2024-12-04 07:59:57 +0100

    d4p1

--- src/04/04.ts
+++ src/04/04.ts
@@ -1,4 +1,4 @@
-import { lines, range, sum, transpose } from '@/advent'
+import { lines, range, sum, tails, transpose } from '@/advent'
 
 export function parse(input: string) {
   return lines(input).map(i => [...i])
@@ -6,108 +6,105 @@ export function parse(input: string) {
 
 type Input = ReturnType<typeof parse>
 
-const inBounds = (coords: [number, number], bounds: [number, number]) =>
-  coords[0] >= 0 &&
-  coords[0] < bounds[0] &&
-  coords[1] >= 0 &&
-  coords[1] < bounds[1]
-
-function* coords(
-  from: [number, number],
-  bounds: [number, number],
-  step: (_: [number, number]) => [number, number]
-): Iterable<[number, number]> {
-  let [sRow, sCol] = from
-
-  while (inBounds([sRow, sCol], bounds)) {
-    yield [sRow, sCol]
-    const [nRow, nCol] = step([sRow, sCol])
-    sRow = nRow
-    sCol = nCol
-  }
-  yield [sRow, sCol]
-}
-
-const getBounds = (input: Input): [number, number] => [
-  input.length,
-  input.at(0)!.length
-]
-
-const getLetter = (input: Input, coords: [number, number]) =>
-  input.at(coords[0])?.at(coords[1])
-
-function trace(input: Input, steps: [number, number][]): number {
-  let total = 0
-  for (let i = 0; i < steps.length; i++) {
-    const word = steps
-      .slice(i, i + 4)
-      .map(c => getLetter(input, c))
-      .join('')
-    if (word === 'XMAS') {
-      total += 1
-    }
-  }
-  return total
-}
-
 function rotate90(input: Input) {
   return transpose(input)
 }
 
+const inBounds = (input: Input, [row, col]: [number, number]) =>
+  row >= 0 && row < input.length && col >= 0 && col < input[0]!.length
+
+function rotate45(input: Input): Input {
+  const step = ([row, col]: [number, number]): [number, number] => [
+    row + 1,
+    col + 1
+  ]
+
+  const result: Input = []
+  for (let iCol = input.at(0)!.length - 1; iCol >= 0; iCol--) {
+    let pos: [number, number] = [0, iCol]
+    let row: string[] = []
+    while (inBounds(input, pos)) {
+      row.push(input.at(pos[0])!.at(pos[1])!)
+      let [nr, nc] = step(pos)
+      pos = [nr, nc]
+    }
+    result.push(row)
+  }
+
+  for (let iRow = 1; iRow < input.length; iRow++) {
+    let pos: [number, number] = [iRow, 0]
+    let row: string[] = []
+    while (inBounds(input, pos)) {
+      row.push(input.at(pos[0])!.at(pos[1])!)
+      let [nr, nc] = step(pos)
+      pos = [nr, nc]
+    }
+    result.push(row)
+  }
+  return result
+}
+
+function rotateMinus45(input: Input): Input {
+  const step = ([row, col]: [number, number]): [number, number] => [
+    row - 1,
+    col + 1
+  ]
+
+  const result: Input = []
+  for (let iRow = 0; iRow < input.length; iRow++) {
+    let pos: [number, number] = [iRow, 0]
+    let row: string[] = []
+    while (inBounds(input, pos)) {
+      row.push(input.at(pos[0])!.at(pos[1])!)
+      let [nr, nc] = step(pos)
+      pos = [nr, nc]
+    }
+    result.push(row)
+  }
+
+  for (let iCol = 1; iCol < input.at(0)!.length; iCol++) {
+    let pos: [number, number] = [input.length - 1, iCol]
+    let row: string[] = []
+    while (inBounds(input, pos)) {
+      row.push(input.at(pos[0])!.at(pos[1])!)
+      let [nr, nc] = step(pos)
+      pos = [nr, nc]
+    }
+    result.push(row)
+  }
+  return result
+}
+
+function count(input: Input): number {
+  return sum(
+    input.flatMap(row => {
+      const line = row.join('')
+      let total = 0
+      for (const tail of tails(line)) {
+        if (tail.startsWith('XMAS')) {
+          total += 1
+        }
+      }
+
+      const reversed = [...row].reverse()
+
+      for (const tail of tails(reversed.join(''))) {
+        if (tail.startsWith('XMAS')) {
+          total += 1
+        }
+      }
+      return total
+    })
+  )
+}
+
 export function partOne(input: Input) {
-  const bounds = getBounds(input)
-  const [rows, cols] = bounds
-  const horizontal = sum(
-    [...range(0, rows)].flatMap(r => [
-      trace(input, [...coords([r, 0], bounds, ([r, c]) => [r, c + 1])]), // right
-      trace(input, [...coords([r, cols - 1], bounds, ([r, c]) => [r, c - 1])]) // left
-    ])
-  )
-
-  const vertical = sum(
-    [...range(0, cols)].flatMap(c => [
-      trace(input, [...coords([0, c], bounds, ([r, c]) => [r + 1, c])]), // down
-      trace(input, [...coords([rows - 1, c], bounds, ([r, c]) => [r - 1, c])]) // up
-    ])
-  )
-
-  const rowsRight = sum(
-    [...range(0, rows)].flatMap(r => [
-      trace(input, [...coords([r, 0], bounds, ([r, c]) => [r + 1, c + 1])]), // down right
-      trace(input, [...coords([r, 0], bounds, ([r, c]) => [r - 1, c + 1])]) // up right
-    ])
-  )
-
-  const rowsLeft = sum(
-    [...range(0, rows)].flatMap(r => [
-      trace(input, [
-        ...coords([r, cols - 1], bounds, ([r, c]) => [r + 1, c - 1])
-      ]), // down left
-      trace(input, [
-        ...coords([r, cols - 1], bounds, ([r, c]) => [r - 1, c - 1])
-      ]) // up left
-    ])
-  )
-
-  const colsUp = sum(
-    [...range(0, cols - 1)].flatMap(c => [
-      trace(input, [
-        ...coords([rows - 1, c], bounds, ([r, c]) => [r - 1, c + 1])
-      ]), // up right
-      trace(input, [
-        ...coords([rows - 1, c], bounds, ([r, c]) => [r - 1, c - 1])
-      ]) // up left
-    ])
-  )
-
-  const colsDown = sum(
-    [...range(1, cols)].flatMap(c => [
-      trace(input, [...coords([0, c], bounds, ([r, c]) => [r + 1, c + 1])]), // down right
-      trace(input, [...coords([0, c], bounds, ([r, c]) => [r + 1, c - 1])]) // down left
-    ])
-  )
-
-  return sum([horizontal, vertical, rowsRight, rowsLeft, colsDown, colsUp])
+  return sum([
+    count(input),
+    count(rotate90(input)),
+    count(rotate45(input)),
+    count(rotateMinus45(input))
+  ])
 }
 
 export function partTwo(input: Input) {}
