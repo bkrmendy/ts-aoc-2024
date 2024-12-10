e6f303540d9cc6a82a2997d4b074148b633f7174
Bertalan Körmendy <korber314@gmail.com>
2024-12-10 07:27:50 +0100

    d10

--- src/move2d.ts
+++ src/move2d.ts
@@ -8,6 +8,25 @@ export interface Facing {
   v: number
 }
 
+interface Bounds {
+  rows: number
+  cols: number
+}
+
+export const getBounds = <T>(ts: T[][]): Bounds => ({
+  rows: ts.length,
+  cols: ts[0]?.length ?? 0
+})
+
+export const inbounds = (bounds: Bounds, position: Position): boolean => {
+  return (
+    0 <= position.r &&
+    position.r < bounds.rows &&
+    0 <= position.c &&
+    position.c < bounds.cols
+  )
+}
+
 export const UP: Facing = { h: 0, v: -1 }
 export const DOWN: Facing = { h: 0, v: 1 }
 export const LEFT: Facing = { h: -1, v: 0 }
@@ -31,3 +50,12 @@ export const fromKey = (key: string): Position => {
 
 export const positionsEqual = (p1: Position, p2: Position): boolean =>
   p1.r === p2.r && p1.c === p2.c
+
+export function neighborsViaEdge(pos: Position, bounds: Bounds): Position[] {
+  return [
+    { r: pos.r - 1, c: pos.c },
+    { r: pos.r + 1, c: pos.c },
+    { r: pos.r, c: pos.c - 1 },
+    { r: pos.r, c: pos.c + 1 }
+  ].filter(p => inbounds(bounds, p))
+}
