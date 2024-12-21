93ddf031293d8e37fe59e19f265cab05347f21a2
Bertalan Körmendy <korber314@gmail.com>
2024-12-21 15:06:41 +0100

    assertNever

--- src/advent.ts
+++ src/advent.ts
@@ -146,3 +146,7 @@ export const mod = (n: number, m: number) => {
   }
   return nn % m
 }
+
+export function assertNever(n: never): never {
+  throw new Error(`Expected never, got: ${JSON.stringify(n)}`)
+}
